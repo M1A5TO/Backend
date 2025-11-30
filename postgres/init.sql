2a728f98e9dd08ea7f37124c72892af2b54acc7d
Kajetan Powała <powala.kajetan11@gmail.com>
2025-11-30 14:46:51 +0100

    refactor: remove unused Redis service and optimize docker-compose setup

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,1 +1,14 @@
-CREATE EXTENSION postgis;
+CREATE EXTENSION IF NOT EXISTS postgis;
+
+-- Tworzenie użytkownika root z pełnymi uprawnieniami
+CREATE ROLE root WITH LOGIN PASSWORD 'root123' SUPERUSER CREATEDB CREATEROLE;
+
+
+INSERT INTO spatial_ref_sys (srid, auth_name, auth_srid, proj4text, srtext)
+VALUES (
+    4326, 
+    'EPSG', 
+    4326, 
+    '+proj=longlat +datum=WGS84 +no_defs',
+    'GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]]'
+) ON CONFLICT (srid) DO NOTHING;
